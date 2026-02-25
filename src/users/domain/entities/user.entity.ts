5261b14477be64fa7936c43425d622431e28000b
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2026-02-24 21:02:24 -0300

    feat: create user entity with varchar and unique constraints

---
+++ src/users/domain/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  name!: string;
+
+  @Column({ type: 'varchar', length: 50, unique: true })
+  username!: string;
+
+  @Column({ type: 'varchar', length: 100, unique: true })
+  email!: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password!: string;
+}
