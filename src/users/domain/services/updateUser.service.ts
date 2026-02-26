36ecbb46b1ed6b6b32fa594818d5b1f465b7f9ae
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2026-02-25 22:43:29 -0300

    feat(users): implement update user service

---
+++ src/users/domain/services/updateUser.service.ts
@@ -0,0 +1,22 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { User } from '../entities/user.entity';
+import * as IusersRepository from '../repositories/Iusers.repository';
+import { UpdateUserDTO } from '../dto/updateUser.dto';
+
+@Injectable()
+export class UpdateUserService {
+  constructor(
+    @Inject('REPOSITORY_TOKEN_USER')
+    private readonly usersRepository: IusersRepository.IUsersRepository,
+  ) {}
+
+  async execute(id: number, data: UpdateUserDTO): Promise<User> {
+    const userExists = await this.usersRepository.findById(id);
+
+    if (!userExists) {
+      throw new NotFoundException('User not found');
+    }
+
+    return this.usersRepository.update(id, data);
+  }
+}
