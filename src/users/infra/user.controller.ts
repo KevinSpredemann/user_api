13c57e57929648ff563338aee76b2819f605e598
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2026-02-26 21:50:35 -0300

    feat(auth): add AuthGuard to protected routes

--- src/users/infra/user.controller.ts
+++ src/users/infra/user.controller.ts
@@ -13,14 +13,14 @@ import {
 import { CreateUserService } from '../domain/services/createUser.service';
 import { UpdateUserService } from '../domain/services/updateUser.service';
 import { GetAllUsersService } from '../domain/services/getAllUsers.service';
-import { GetByEmailUserService } from '../domain/services/GetByEmailUser.service';
+import { GetByEmailUserService } from '../domain/services/getByEmailUser.service';
 import { GetUserByIdService } from '../domain/services/getUserById.service';
 import { DeleteUserService } from '../domain/services/deleteUser.service';
 import { CreateUserDTO } from '../domain/dto/createUser.dto';
 import { JwtAuthGuard } from '../../auth/guards/jwtAuth.guard';
 import { UpdateUserDTO } from '../domain/dto/updateUser.dto';
+import { UserResponseDTO } from '../domain/dto/userReponse.dto';
 
-@UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UserController {
   constructor(
@@ -32,30 +32,36 @@ export class UserController {
     private deleteUserService: DeleteUserService,
   ) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   getAll() {
     return this.getAllUserService.execute();
   }
 
-  @Get(':id')
-  getById(@Param('id', ParseIntPipe) id: number) {
-    return this.getByIdUserService.execute(id);
-  }
-
+  @UseGuards(JwtAuthGuard)
   @Get('/email')
   getByEmail(@Query('email') email: string) {
     return this.getByEmailUserService.execute(email);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  getById(@Param('id', ParseIntPipe) id: number) {
+    return this.getByIdUserService.execute(id);
+  }
+
   @Post()
-  create(@Body() body: CreateUserDTO) {
+  create(@Body() body: CreateUserDTO): Promise<UserResponseDTO> {
     return this.createUserService.execute(body);
   }
+
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   delete(@Param('id') id: number) {
     return this.deleteUserService.execute(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id', ParseIntPipe) id: number, @Body() body: UpdateUserDTO) {
     return this.updateUserService.execute(id, body);
